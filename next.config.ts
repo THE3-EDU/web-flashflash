5a683f572ff5f270514b534e96492febb7fac4fe
Lizzy-Su <1033876562@qq.com>
2026-01-31 22:12:25 +0800

    Add basePath for GitHub Pages subpath deployment

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // 如果 GitHub Pages 部署在根路径 (https://the3-edu.github.io)，不需要 basePath
-  // 如果部署在子路径 (https://the3-edu.github.io/web-flashflash)，取消下面的注释
-  // basePath: '/web-flashflash',
-  // assetPrefix: '/web-flashflash',
+  basePath: '/web-flashflash',
+  assetPrefix: '/web-flashflash',
   images: {
     unoptimized: true,
   },
